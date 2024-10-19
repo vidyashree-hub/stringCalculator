71c9dacd01ccf9f07b3a1f12691189bd9c61e768
Vidyashree <vshree227@gmail.com>
2024-10-19 21:01:14 +0530

    functionality to ignore more than 1000 number

--- stringValueCalculator.js
+++ stringValueCalculator.js
@@ -7,15 +7,20 @@ const numArray = inputData.split(' ').filter(num=>num!='');
 const numbers = numArray.map(num => parseFloat(num)) //convert to number
 //check for negative values
 const negativeNumbers = numbers.filter(num => num<0)
-
+//filter values for than 1000
+const largeNumbers = numbers.filter(num => num >= 1000);
 if(negativeNumbers.length>0){
     throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(', ')}`)
+}
+
+if (largeNumbers.length > 0) {
+  throw new Error(`Values starting with 1000 series not allowed: ${largeNumbers.join(', ')}`);
 }
 const totalSum = numbers.reduce((sum,num)=>sum+num,0)
 return totalSum;
 }
 
-const inputValue = "//;\n1;2\n3,4;5\n-1;-2;-3";
+const inputValue = "//;\n1;2\n3,4;5\n1000;1001";
 try{
 const result = add(inputValue)
 }catch(error){
